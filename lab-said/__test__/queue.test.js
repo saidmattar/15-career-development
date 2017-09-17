58021319830a64a60aa4434f9b85240f09a11b77
saidmattar <ssabdou@gmail.com>
2017-09-17 01:45:27 -0700

    queue tests something not right!

---
+++ lab-said/__test__/queue.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Queue = require('../lib/queue');
+require('jest');
+
+describe('queue methods', () => {
+
+  describe('queue methods', () => {
+    describe('enqueue', () => {
+      test('should add new node to the end of a Queue', () => {
+        let result = new Queue;
+        result.enqueue(1);
+        expect(result).toEqual(1);
+      });
+    });
+    describe('dequeue', () => {
+      test('should remove the first in node in the front of the queue', () => {
+        let deqResult = new Queue;
+        deqResult.enqueue(1);
+        deqResult.enqueue(2);
+        deqResult.dequeue();
+        expect(deqResult).toEqual(2);
+      });
+    });
+  });
+});
